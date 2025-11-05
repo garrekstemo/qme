1f5475a3778561b6a89f40c676f2be0ca60a8f61
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-11-05 14:04:42 +0900

    Clarify column-major ordering in Julia vs row-major ordering in Python

--- julia/3.1_vectorizing_rho.jl
+++ julia/3.1_vectorizing_rho.jl
@@ -1,7 +1,11 @@
 using LinearAlgebra
 
+# Note that Julia uses column-major order,
+# so the result will differ from that in Python (which uses row-major order).
+# Any reshaping is valid as long as it is consistent with subsequent operations.
+
 ψ = [1, 2im, 0, -2, 0] # some (non-normalized) state
-ρ = ψ * ψ'
-ρ /= tr(ρ)
-d = length(ψ)
-ρ_vec = reshape(ρ, d^2, 1) # vectorized density matrix
+ρ = ψ * ψ'  # its density matrix
+ρ /= tr(ρ)  # normalized density matrix
+d = length(ψ)  # the dimension of the system's space
+ρ_vec = vec(ρ) # vectorized density matrix
