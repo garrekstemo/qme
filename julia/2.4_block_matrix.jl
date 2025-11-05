f7da64cd9a6538b5145d10cf3dc3256803fe6c3f
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-11-05 13:46:21 +0900

    Clarify how identity operator works in Julia

--- julia/2.4_block_matrix.jl
+++ julia/2.4_block_matrix.jl
@@ -1,5 +1,8 @@
 using LinearAlgebra
 
-# LinearAlgebra is only needed for I(n).
+# LinearAlgebra is only needed for identity matrix `I`.
+# Note that `I(n)` creates an n x n identity matrix,
+# but `I` acts like an identity matrix for any compatible size.
+
 d_a, d_b = 2, 3 # dimensions of the bases
-basis = [I(d_a) zeros(d_a, d_b); zeros(d_b, d_a) I(d_b)] # block matrix
+basis = [I zeros(d_a, d_b); zeros(d_b, d_a) I] # block matrix
