77d72e985c7aae442e9e539cf008e25bac8cc4d8
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-11-07 17:04:31 +0900

    Improve variable naming; enhance plot labels for clarity

--- julia/3.9_suzuki.jl
+++ julia/3.9_suzuki.jl
@@ -4,37 +4,38 @@ include("src/liouvillian.jl")
 
 σx = [0 1; 1 0]
 σy = [0 -im; im 0]
-σz = [1 0; 0 -1]
-σp = σx + im * σy / 2
-σm = σx - im * σy / 2
+σplus = (σx + im * σy) / 2
+σminus = (σx - im * σy) / 2
 
-superop_1 = liouvillian(σx, [σp])
-superop_2 = liouvillian(σy, [σm])
+superop_1 = liouvillian(σx, [σplus])
+superop_2 = liouvillian(σy, [σminus])
 ρ0 = [1 0; 0 0]
-d = size(ρ0, 1) # dimension of state and superoperator
-ms = [50, 100, 1000]
+d = size(ρ0, 1) # dimension of the state
 
 fig = Figure()
-ax = Axis(fig[1, 1], xlabel = "t", ylabel = "Tr[ρ(t)ρ(0)]")
+ax = Axis(fig[1, 1], xlabel = "t", ylabel = "Tr[ρ(t)ρ(‘0)]")
+
+ms = [50, 100, 1000]
+alphas = [0.4, 0.7, 1.0]
 
 for (km, m) in enumerate(ms)
-    dt = 10/m
+    dt = 10 / m  # time step
     P_1 = exp(superop_1 * dt)
     P_2 = exp(superop_2 * dt)
-    P = P_2 * P_1 # Suzuki-Trotter expansion for small times dt
-    times_0 = []
-    populations_0 = []
+    P = P_1 * P_2 # Suzuki-Trotter expansion for small times dt
+
     t = 0.0
     ρ = ρ0
-
+    times_0 = []
+    populations_0 = []
     for k in 1:m
         push!(populations_0, tr(ρ * ρ0))
         push!(times_0, t)
+        ρ = reshape(P * vec(ρ), d, d)
         t += dt
-        ρ = reshape(P * reshape(ρ, d^2), d, d)
     end
 
-    lines!(ax, times_0, real(populations_0), label = "m = $m")
+    lines!(ax, times_0, real(populations_0), color = :deepskyblue4, alpha = alphas[km], label = "m = $m")
 end
 
 axislegend(ax)
