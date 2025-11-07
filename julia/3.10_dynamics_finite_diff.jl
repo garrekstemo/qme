80878d379322ca4646495063b639d3d2fe6836a6
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-11-07 17:19:27 +0900

    Add SVD approach and update plots to show both

--- julia/3.10_dynamics_finite_diff.jl
+++ julia/3.10_dynamics_finite_diff.jl
@@ -15,8 +15,19 @@ L = sqrt(Γ) * [0 1; 1 0]
 superop = liouvillian(H_s, [L])
 
 # Here we must explicitly declare the state as complex
+# to work with the DifferentialEquations.jl package
 ρ0_vec = ComplexF64[0, 0, 0, 1]
 
+# First repeat the SVD approach from 3.6
+ts = range(0, 200, length = 200)
+ρt_vec = zeros(length(ρ0_vec), length(ts))
+
+for k in 1:d^2
+    norm = left[:, k]' * right[:, k]
+    ρt_vec += (left[:, k]' * ρ0_vec * right[:, k]) * exp.(λ[k] * ts)' / norm
+end
+
+# Now do the finite difference approach using DifferentialEquations.jl
 t0 = 0.0
 tf = 200.0
 times = range(t0, tf, 20)
@@ -28,9 +39,11 @@ tspan = (t0, tf)
 prob = ODEProblem(f, ρ0_vec, tspan)
 sol = solve(prob, RK4(), saveat = times)
 
+
 fig = Figure()
 ax = Axis(fig[1, 1], xlabel = "time (tΩ)", ylabel = "Tr[ρ(t)ρ₀]")
-lines!(times .* Ω, real.(sol[4, :]), label = "RK4")
+lines!(ts * Ω, real(ρt_vec[4, :]), label = "SVD")
+scatter!(times .* Ω, real.(sol[4, :]), marker = :rect, color = :black, label = "RK4")
 
 axislegend(ax)
 fig
