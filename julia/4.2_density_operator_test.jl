6a266487adde160dd604a87981e16652eee69c62
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2025-11-17 17:21:18 +0900

    Minor formatting changes

--- julia/4.2_density_operator_test.jl
+++ julia/4.2_density_operator_test.jl
@@ -1,11 +1,12 @@
 using LinearAlgebra
 
 function is_state(ρ)
-    vals = eigvals(ρ)
-    non_unit = 1 - tr(ρ)
-    non_hermitian = norm(ρ - ρ')
-    non_positive =  sum((abs.(vals) .- vals) / 2)
+    vals = eigvals(ρ)  # eigenvalues of ρ
+    non_unit = 1 - tr(ρ)  # deviation from unit trace
+    non_hermitian = norm(ρ - ρ')  # deviation from Hermiticity
+    non_positive =  sum((abs.(vals) .- vals) / 2)  # deviation from positivity
 
+    # return 1 if ρ is a valid density matrix, else return a value < 1
     return 1 - norm([non_unit, non_hermitian, non_positive])
 end
 
@@ -13,10 +14,9 @@ end
 ρ_1 = [0.2 0 0
        0 0.3 0
        0 0 0.5]
-is_state(ρ_1) # should return 1
 
 # a state with some error
 ρ_2 = ρ_1 + [-1e-4 1e-6 0; 0 0 1e-2im; 1e-3im 0 1e-4]
-is_state(ρ_2) # should return something close to 1
 
-# Note: eigvals is evaluating very small imaginary eigenvalues to zero in this example.
+println("Valid state metric (ρ_1): ", is_state(ρ_1))
+println("Valid state metric (ρ_2): ", is_state(ρ_2))
